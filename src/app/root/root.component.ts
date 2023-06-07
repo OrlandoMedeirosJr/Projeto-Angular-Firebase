cac80a7e2296bc789dec708f5cd6bb6764403ddf
OrlandoMedeirosJr <118356375+OrlandoMedeirosJr@users.noreply.github.com>
2023-06-06 21:53:34 -0300

    Projeto Adoção-Angular

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, SnapshotAction } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
-import { map } from 'rxjs/operators';
 
-export interface Item {
-  key: string;
-  email: string;
-  name: string;
-  age: number;
+interface Animal {
+  nome: string;
+  tipo: string;
+  foto: string;
+  selecionado: boolean;
+}
+
+interface FormData {
+  nome: string;
+  cpf: string;
+  telefone: string;
 }
 
 @Component({
@@ -17,37 +20,55 @@ export interface Item {
   styleUrls: ['./root.component.css']
 })
 export class RootComponent implements OnInit {
-  email = '' as string;
-  password = '' as string;
+  email = '';
+  password = '';
 
-  formNewName = '' as string;
-  formNewAge = '' as string;
+  animals: Animal[] = [
+    { nome: 'Jack Sparrow', tipo: 'Gato', foto: 'https://www.petz.com.br/blog/wp-content/uploads/2022/02/gato-e-carnivoro2.jpg', selecionado: false },
+    { nome: 'Harry Potter', tipo: 'Gato', foto: 'https://www.manzoni.com.br/wp-content/uploads/2023/01/topo-blog2.jpg', selecionado: false },
+    { nome: 'Voldemort', tipo: 'Cachorro', foto: 'https://razoesparaacreditar.com/wp-content/uploads/2015/02/DARLA.jpg', selecionado: false },
+    { nome: 'Pikachu', tipo: 'Cachorro', foto: 'https://sonhoastral.com/uploads/article/image/2947/1111.png', selecionado: false },
+    { nome: 'Kakaroto', tipo: 'Cachorro', foto: 'https://blog.polipet.com.br/wp-content/uploads/2022/07/pitbull.jpg', selecionado: false }
+  ];
 
-  listRef: any;
-  list: Observable<Item[]>;
+  formData: FormData = {
+    nome: '',
+    cpf: '',
+    telefone: ''
+  };
 
-  constructor(public auth: AuthService, private database: AngularFireDatabase) {
-    this.listRef = database.list('list');
-    this.list = this.listRef.snapshotChanges().pipe(
-      map((changes: SnapshotAction<Item>[]) => 
-        changes.map((c) => ({key: c.payload.key,  ...c.payload.val()}))
-        )
-      );
-   }
+  adocaoConfirmada = false;
+
+  constructor(public auth: AuthService) {}
 
   ngOnInit() {}
 
-  addItem() {
-    this.listRef.push({
-      name: this.formNewName,
-      age: this.formNewAge,
-      email: this.auth.user.email,
-    });
-    this.formNewName = '';
-    this.formNewAge = '';
-  }
-
-  deleteItem(key: string) {
-    this.listRef.remove(key);
+  submitForm() {
+    // Validar os campos do formulário
+    if (!this.formData.nome || !this.formData.cpf || !this.formData.telefone) {
+      alert('Por favor, preencha todos os campos do formulário.');
+      return;
     }
+
+    // Realizar a lógica de envio do formulário, como salvar no Firebase ou em outro local
+    // ...
+
+    // Limpar os campos do formulário após o envio
+    this.formData = {
+      nome: '',
+      cpf: '',
+      telefone: ''
+    };
+
+    // Remover animais selecionados da lista de adoção
+    const selectedAnimals = this.animals.filter(animal => animal.selecionado);
+    selectedAnimals.forEach(animal => {
+      const index = this.animals.findIndex(a => a === animal);
+      if (index !== -1) {
+        this.animals.splice(index, 1);
+      }
+    });
+
+    this.adocaoConfirmada = true;
+  }
 }
